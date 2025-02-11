b19f26b38b90b5db9f176172f79e6749a617b5c7
ClaireBruce <bobtherainbowface@gmail.com>
2025-02-11 19:08:25 +0000

    Added API lookup for weather forecast

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function fetchWeather(response) {
+function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
@@ -9,10 +9,6 @@ function fetchWeather(response) {
     let timeElement = document.querySelector("#time");
     let iconElement = document.querySelector("#icon");
     
-    
-    
-
-    console.log(response.data);
 
     temperatureElement.innerHTML = Math.round(temperature);
     descriptionElement.innerHTML = capitalize(response.data.condition.description);
@@ -21,7 +17,9 @@ function fetchWeather(response) {
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
-}
+
+    fetchForecast(response.data.city);
+  }
 
 function formatDate(date) {
     let minutes = date.getMinutes();
@@ -60,7 +58,7 @@ text :
 function searchCity(city) {
     let apiKey = "03a1od824e34f4b433a55ct759776fa6";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(fetchWeather);
+    axios.get(apiUrl).then(refreshWeather);
 }
 
 function searchSubmit(event) {
@@ -68,6 +66,43 @@ function searchSubmit(event) {
     let searchInput =  document.querySelector("#search-form-input");
 
     searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function fetchForecast(city) {
+  let apiKey = "03a1od824e34f4b433a55ct759776fa6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+    forecastHtml =
+      forecastHtml + 
+      `
+    <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+                <strong>${Math.round(day.temperature.maximum)}°</strong></div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+        </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
